3d527cd954b0c5a64ed410c43607c25e08477ed0
Pinapelz <donaldshan1@outlook.com>
2023-12-13 22:39:33 -0800

    fix: re-add missing closing div

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -148,6 +148,7 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
             btnText={t('Customise link')}
             btnIcon="pen"
           />
+          </div>
       </DownloadBtnContainer>
     </>
   )
